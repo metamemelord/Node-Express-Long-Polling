8c2be30c1edfcdac93023b85faf6c55b93acc6a0
Gaurav Saini <sainigaurav.2009@gmail.com>
2019-04-07 16:37:49 +0530

    Added maps integration to show realtime updates

--- app.js
+++ app.js
@@ -17,17 +17,23 @@ app.get("/poll", (req, res) => {
   emitter.on("rand", data => {
     if (!res.headersSent) {
       res.status(200).send(data);
+      emitter.removeAllListeners();
     }
   });
   setTimeout(() => {
     if (!res.headersSent) {
       res.status(204).send();
+      emitter.removeAllListeners();
     }
-  }, 250);
+  }, 500);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
+  res.redirect("/");
 });
 
 const port = process.env.PORT || 4000;
@@ -41,5 +47,10 @@ app.listen(port, err => {
 });
 
 setInterval(() => {
-  emitter.emit("rand", { data: 100 * Math.random() });
-}, Math.ceil(5000 * Math.random()));
+  let interval = setInterval(() => {
+    emitter.emit("rand", {
+      delta: { lat: 0.001 * Math.random(), lon: 0.001 * Math.random() }
+    });
+    clearInterval(interval);
+  }, Math.ceil(2000 * Math.random()));
+}, 500);
---
+++ client/script.js
@@ -0,0 +1,73 @@
+var map = L.map("map");
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var target = L.latLng("28.7041", "77.1025");
+map.setView(target, 14);
+
+var theMarker = {};
+
+theMarker = L.marker([28.7041, 77.1025]).addTo(map);
+
+new Vue({
+  el: "#app",
+  data: {
+    ping_frequency: 2000,
+    interval_id: -1,
+    messages: [],
+    coords: [28.7041, 77.1025]
+  },
+  methods: {
+    startPolling() {
+      this.interval_id = setInterval(() => {
+        const start = Date.now();
+        fetch("poll")
+          .then(res => {
+            if (res.status === 200) {
+              return res.json();
+            } else {
+              return Promise.resolve("No changes!");
+            }
+          })
+          .then(data => {
+            if (typeof data === "object") {
+              map.removeLayer(theMarker);
+              if (Math.floor(2 * Math.random())) {
+                this.coords[0] += data.delta.lat;
+                this.coords[1] += data.delta.lon;
+              } else {
+                this.coords[0] -= data.delta.lat;
+                this.coords[1] -= data.delta.lon;
+              }
+              theMarker = L.marker(this.coords).addTo(map);
+            }
+            const current_time = new Date();
+            const message = {
+              time_taken: current_time.getTime() - start,
+              data,
+              ts: moment(current_time).format("hh:mm:ss A")
+            };
+            this.messages.push(message);
+          })
+          .catch(error => {
+            console.log(error);
+            const current_time = new Date();
+            this.messages.push({
+              time_taken: current_time.getTime() - start,
+              data: "Error: Could not reach the server! :(",
+              ts: moment(current_time).format("hh:mm:ss A")
+            });
+          });
+      }, this.ping_frequency);
+    },
+    stopPolling() {
+      clearInterval(this.interval_id);
+      this.interval_id = -1;
+    },
+    clearMessages() {
+      this.messages = [];
+    }
+  }
+});
